9139a6ccdddedc8038be4d40ce0099bc382b5c0a
SaadEla <sa.elaouani@gmail.com>
2020-02-13 14:41:02 +0100

    ContextAndHooks

--- src/App.js
+++ src/App.js
@@ -4,6 +4,8 @@ import Navbar from './components/Navbar';
 import ThemeContextProvider from './contexts/ThemeContext';
 import ThemeToggle from './components/ThemeToggle';
 import AuthContextProvider from './contexts/AuthContext';
+import BookContextProvider from './contexts/BookContext';
+
 
 function App() {
   //Navbar et BookList sont attachés aux props du ThemeContextProvider
@@ -17,7 +19,9 @@ function App() {
     <ThemeContextProvider>
       <AuthContextProvider>
         <Navbar />
-        <BookList />
+        <BookContextProvider>
+          <BookList />
+        </BookContextProvider>
         <ThemeToggle />
       </AuthContextProvider>
     </ThemeContextProvider>
--- src/components/BookList.js
+++ src/components/BookList.js
@@ -1,21 +1,25 @@
-import React, { Component } from 'react';
+import React, { useContext} from 'react';
 import { ThemeContext } from '../contexts/ThemeContext';
+import { BookContext } from '../contexts/BookContext';
 
-class BookList extends Component {
-  static contextType = ThemeContext;
-  render() { 
-    const { isLightTheme, light, dark } = this.context;
-    const theme = isLightTheme ? light : dark;
-    return ( 
-      <div className="book-list" style={{ color: theme.syntax, background: theme.bg }}>
-        <ul>
-          <li style={{ background: theme.ui }}>the way of kings</li>
-          <li style={{ background: theme.ui }}>the name of the wind</li>
-          <li style={{ background: theme.ui }}>the final empire</li>
-        </ul>
-      </div>
-    );
-  }
+//NAAADI HAD CONTEXT M3A USECONTEXT HOOK
+const BookList = () => {
+  const {isLightTheme, light, dark} = useContext(ThemeContext);
+  const { books } = useContext(BookContext)
+  const theme = isLightTheme ? light : dark;
+  const book = books.map( (xx) =>{
+    return(
+      <li style={{ background: theme.ui }} key={xx.id}>{xx.title}</li>
+    )
+  })
+  return (     
+    <div className="book-list" style={{ color: theme.syntax, background: theme.bg }}>
+      <ul>
+        {book}
+      </ul>
+    </div>
+  );
 }
- 
+
 export default BookList;
+
