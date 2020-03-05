cf52bb04dcd4dca6c9b4b3545974b163aa800296
SaadEla <sa.elaouani@gmail.com>
2020-03-05 14:38:32 +0100

    "react-animations"

--- src/components/BookList.js
+++ src/components/BookList.js
@@ -1,7 +1,15 @@
 import React, { useContext} from 'react';
 import { ThemeContext } from '../contexts/ThemeContext';
 import { BookContext } from '../contexts/BookContext';
+import { fadeOutRight } from 'react-animations';
+import Radium, { StyleRoot }from 'radium';
 
+const styles = {
+  fadeOutRight:{
+    animation: 'x 1s',
+    animationName: Radium.keyframes(fadeOutRight, 'bounce')
+  }
+}
 //NAAADI HAD CONTEXT M3A USECONTEXT HOOK
 const BookList = () => {
   const {isLightTheme, light, dark} = useContext(ThemeContext);
@@ -11,13 +19,32 @@ const BookList = () => {
     return(
       <li style={{ background: theme.ui }} key={xx.id}>{xx.title}</li>
     )
-  })
-  return (     
-    <div className="book-list" style={{ color: theme.syntax, background: theme.bg }}>
-      <ul>
-        {book}
-      </ul>
-    </div>
+  });
+  let bool = true;
+  const declencher = ()=>{
+    bool = !bool;
+    console.log(bool)
+  }
+  return (   
+    <StyleRoot>
+      <div>  
+        <div className="book-list" style={{ color: theme.syntax, background: theme.bg }}>
+          <ul>
+            {book}
+          </ul>
+        </div>
+        { bool ? 
+                <div>
+                  <h1>React Animation</h1>
+                </div>
+                :
+                <div style={styles.fadeOutRight}>
+                  <h1>React Animation</h1>
+                </div>
+        }
+        <button onClick={declencher} >Declencher</button>
+      </div>
+    </StyleRoot>
   );
 }
 
